eb1af715cd7ae8538c1e61e40519511a5c948b2f
terryshu <terry.shu@hotmail.com>
2019-05-19 23:36:03 -0400

    update

--- src/helper/utilities.py
+++ src/helper/utilities.py
@@ -10,30 +10,36 @@ import numpy as np
 
 PRE, REC, SPEC, FPR, NPV, ACC, F1 = 7, 6, 5, 4, 3, 2, 1
 
-def _randint(a=0,b=0):
-    return randint(a,b)
+
+def _randint(a=0, b=0):
+    return randint(a, b)
+
 
 def _randchoice(a=[]):
     return choice(a)
 
-def _randuniform(a=0.0,b=0.0):
-    return uniform(a,b)
 
-def _randsample(a=[],b=1):
-    return sample(a,b)
+def _randuniform(a=0.0, b=0.0):
+    return uniform(a, b)
+
+
+def _randsample(a=[], b=1):
+    return sample(a, b)
+
 
 def unpack(l):
-    tmp=[]
+    tmp = []
     for i in l:
-        if list!=type(i):
+        if list != type(i):
             tmp.append(i)
         else:
             for x in i:
                 tmp.append(x)
     return tmp
 
+
 def get_performance(prediction, test_labels):
-    tn, fp, fn, tp = confusion_matrix(test_labels,prediction, labels=[0,1]).ravel()
+    tn, fp, fn, tp = confusion_matrix(test_labels, prediction, labels=[0, 1]).ravel()
     pre = 1.0 * tp / (tp + fp) if (tp + fp) != 0 else 0
     rec = 1.0 * tp / (tp + fn) if (tp + fn) != 0 else 0
     spec = 1.0 * tn / (tn + fp) if (tn + fp) != 0 else 0
@@ -43,21 +49,22 @@ def get_performance(prediction, test_labels):
     f1 = 2.0 * tp / (2.0 * tp + fp + fn) if (2.0 * tp + fp + fn) != 0 else 0
     return [round(x, 3) for x in [pre, rec, spec, fpr, npv, acc, f1]]
 
-def get_score(criteria, prediction, test_labels,data):
-    tn, fp, fn, tp = confusion_matrix(test_labels,prediction, labels=[0,1]).ravel()
-    pre, rec, spec, fpr, npv, acc, f1 = get_performance(test_labels,prediction)
+
+def get_score(criteria, prediction, test_labels, data):
+    tn, fp, fn, tp = confusion_matrix(test_labels, prediction, labels=[0, 1]).ravel()
+    pre, rec, spec, fpr, npv, acc, f1 = get_performance(test_labels, prediction)
     all_metrics = [tp, fp, tn, fn, pre, rec, spec, fpr, npv, acc, f1]
     if criteria == "Accuracy":
         score = -all_metrics[-ACC]
     elif criteria == "d2h":
         score = all_metrics[-FPR] ** 2 + (1 - all_metrics[-REC]) ** 2
         score = math.sqrt(score) / math.sqrt(2)
-    elif criteria=="Pf_Auc":
-        score=auc_measure(prediction,test_labels)
-    elif criteria=="popt":
-        score=get_auc(data)
-    elif criteria=="popt20":
-        score=get_popt20(data)
+    elif criteria == "Pf_Auc":
+        score = auc_measure(prediction, test_labels)
+    elif criteria == "popt":
+        score = get_auc(data)
+    elif criteria == "popt20":
+        score = get_popt20(data)
     elif criteria == "Gini":
         p1 = all_metrics[-PRE]  # target == 1 for the positive split
         p0 = 1 - all_metrics[-NPV]  # target == 1 for the negative split
@@ -77,6 +84,7 @@ def auc_measure(prediction, test_labels):
     fpr, tpr, _ = roc_curve(test_labels, prediction, pos_label=1)
     auc1 = auc(fpr, tpr)
     return auc1
+
 
 def subtotal(x):
     xx = [0]
@@ -107,45 +115,45 @@ def get_auc(data):
     try:
         ret = round(auc(xx, yy), 3)
     except:
-        #print"?"
+        # print"?"
         ret = 0
     return ret
 
+
 def get_popt20(data):
+    data.sort_values(by=["bug", "loc"], ascending=[0, 1], inplace=True)
+    x_sum = float(sum(data['loc']))
+    x = data['loc'].apply(lambda t: t / x_sum)
+    xx = subtotal(x)
 
-  data.sort_values(by=["bug", "loc"], ascending=[0, 1], inplace=True)
-  x_sum = float(sum(data['loc']))
-  x = data['loc'].apply(lambda t: t / x_sum)
-  xx = subtotal(x)
+    # get  AUC_optimal
+    yy = get_recall(data['bug'].values)
+    xxx = [i for i in xx if i <= 0.2]
+    yyy = yy[:len(xxx)]
+    s_opt = round(auc(xxx, yyy), 3)
 
-  # get  AUC_optimal
-  yy = get_recall(data['bug'].values)
-  xxx = [i for i in xx if i <= 0.2]
-  yyy = yy[:len(xxx)]
-  s_opt = round(auc(xxx, yyy), 3)
+    # get AUC_worst
+    xx = subtotal(x[::-1])
+    yy = get_recall(data['bug'][::-1].values)
+    xxx = [i for i in xx if i <= 0.2]
+    yyy = yy[:len(xxx)]
+    try:
+        s_wst = round(auc(xxx, yyy), 3)
+    except:
+        # print "s_wst forced = 0"
+        s_wst = 0
 
-  # get AUC_worst
-  xx = subtotal(x[::-1])
-  yy = get_recall(data['bug'][::-1].values)
-  xxx = [i for i in xx if i <= 0.2]
-  yyy = yy[:len(xxx)]
-  try:
-    s_wst = round(auc(xxx, yyy), 3)
-  except:
-    # print "s_wst forced = 0"
-    s_wst = 0
+    # get AUC_prediction
+    data.sort_values(by=["prediction", "loc"], ascending=[0, 1], inplace=True)
+    x = data['loc'].apply(lambda t: t / x_sum)
+    xx = subtotal(x)
+    yy = get_recall(data['bug'].values)
+    xxx = [k for k in xx if k <= 0.2]
+    yyy = yy[:len(xxx)]
+    try:
+        s_m = round(auc(xxx, yyy), 3)
+    except:
+        return 0
 
-  # get AUC_prediction
-  data.sort_values(by=["prediction", "loc"], ascending=[0, 1], inplace=True)
-  x = data['loc'].apply(lambda t: t / x_sum)
-  xx = subtotal(x)
-  yy = get_recall(data['bug'].values)
-  xxx = [k for k in xx if k <= 0.2]
-  yyy = yy[:len(xxx)]
-  try:
-    s_m = round(auc(xxx, yyy), 3)
-  except:
-    return 0
-
-  Popt = (s_m - s_wst) / (s_opt - s_wst)
-  return round(Popt,3)
+    Popt = (s_m - s_wst) / (s_opt - s_wst)
+    return round(Popt, 3)
